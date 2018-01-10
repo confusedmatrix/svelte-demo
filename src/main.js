9f9de2bb5048d9b68891cda5ac41c2b654144080
Christopher Briggs <confusedmatrix@gmail.com>
2018-01-10 20:33:54 +0000

    Added any extra field in data.json file

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import data from './data.json'
 const app = new App({
 	target: document.body,
 	data: {
-        name: 'world',
-        modules: data.modules,
+        	name: 'world',
+        	modules: data.modules,
 	}
 });
 
